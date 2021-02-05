d68c64c9b6ee7bd6bd69b9e2b85b966c498f187f
Rizwan <rizwankhalid171@gmail.com>
2021-02-05 21:22:49 +0000

    WIP: trying to scrape recipes for each class from food.com

--- foodRecipeScraper.py
+++ foodRecipeScraper.py
@@ -12,3 +12,22 @@ for foodClass in foodClassesList:
     foodClass = foodClass.replace("_", "+")
     foodClassesListClean.append(foodClass)
 
+# from bs4 import BeautifulSoup
+# import requests
+
+# urlsToScrape
+
+# from recipe_scrapers import scrape_me
+
+# scrapedData = scrape_me("https://www.food.com/recipe/kittencals-italian-melt-in-your-mouth-meatballs-69173")
+
+# print(scrapedData.title())
+# # print(scrapedData.author())
+# for ing in scrapedData.ingredients():
+#     print(ing)
+
+# print(scrapedData.instructions())
+# print(scrapedData.yields())
+# print(scrapedData.total_time())
+# print(scrapedData.image())
+# print(scrapedData.ratings())
