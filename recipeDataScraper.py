c82d499fd78a2426b5f7dc70e5d24a9775b15b7b
Rizwan <rizwankhalid171@gmail.com>
2021-02-07 20:46:20 +0000

    Scraped recipe details and stored them in a recipe_data.json file for initial website seed data

--- recipeDataScraper.py
+++ recipeDataScraper.py
@@ -1,15 +1,49 @@
+import enum
 from recipe_scrapers import scrape_me
+import json
 
-# TODO Testing with one link
-scrapedData = scrape_me("https://www.food.com/recipe/kittencals-italian-melt-in-your-mouth-meatballs-69173")
+# open the file and get each link
+linksFile = open('recipeLinksFiltered.txt', 'r')
+linksData = linksFile.readlines()
 
-print(scrapedData.title())
-# print(scrapedData.author()) this info may not be needed
-for ing in scrapedData.ingredients():
-    print(ing)
+# remove \n from end of each link
+recipeLinks = []
+for link in linksData:
+    link = link.strip("\n")
+    recipeLinks.append(link)
 
-print(scrapedData.instructions())
-print(scrapedData.yields())
-print(scrapedData.total_time())
-print(scrapedData.image())
-print(scrapedData.ratings())
+# open the list of classes and read each line
+foodClassesFile = open('classes.txt', 'r')
+foodClassesList = foodClassesFile.readlines()
+
+# note: first line in file is not a class
+foodClassesList = foodClassesList[1 : len(foodClassesList)]
+
+# cleaning up the class names
+foodClassesListClean = []
+for foodClass in foodClassesList:
+    foodClass = foodClass.strip("\n")
+    foodClass = foodClass.replace("_", " ")
+    foodClass = foodClass.title()
+    foodClassesListClean.append(foodClass)
+
+# json object that will contain recipe info
+recipeDataJSON = {}
+
+jsonFile = open("recipe_data.json", "a")
+
+# for each link, scrape the recipe data and insert it into the json object
+for index, link in enumerate(recipeLinks):
+    scrapedData = scrape_me(link)
+
+    recipeDataJSON["title"] = foodClassesListClean[index]
+    recipeDataJSON["instructions"] = scrapedData.instructions()
+    recipeDataJSON["ingredients"] = scrapedData.ingredients()
+    recipeDataJSON["yields"] = scrapedData.yields()
+    recipeDataJSON["total_time"] = scrapedData.total_time()
+    recipeDataJSON["img"] = scrapedData.image()
+    
+    json.dump(recipeDataJSON, jsonFile)
+    jsonFile.write(",")
+
+jsonFile.close()
